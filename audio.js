b69982518da47e5d74d79a535931568c2f5a0d07
James Girard <dsfreak0109@gmail.com>
2025-10-15 09:23:53 -0400

    Separated Audio from Ducky logic

---
+++ audio.js
@@ -0,0 +1,52 @@
+// audio.js
+export class AudioManager {
+  constructor(audioElementId) {
+    this.audioElement = document.getElementById(audioElementId);
+    this.audioCtx = null;
+    this.analyser = null;
+    this.dataArray = null;
+    this.isInitialized = false;
+
+    this.audioElement.addEventListener("play", () => this.setup());
+  }
+
+  setup() {
+    if (this.isInitialized) return;
+
+    this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    const source = this.audioCtx.createMediaElementSource(this.audioElement);
+
+    this.analyser = this.audioCtx.createAnalyser();
+    this.analyser.fftSize = 512;
+    this.dataArray = new Uint8Array(this.analyser.frequencyBinCount);
+
+    source.connect(this.analyser);
+    this.analyser.connect(this.audioCtx.destination);
+
+    this.audioCtx.resume().then(() => {
+      this.audioElement.play();
+      this.isInitialized = true;
+    });
+  }
+
+  update() {
+    if (!this.analyser) return null;
+    this.analyser.getByteFrequencyData(this.dataArray);
+    return this.dataArray;
+  }
+
+  getBassLevel() {
+    if (!this.dataArray) return 0;
+    const bassBins = this.dataArray.slice(0, 10);
+    return bassBins.reduce((a, b) => a + b, 0) / bassBins.length;
+  }
+
+  getMidHighLevel() {
+    if (!this.dataArray) return 0;
+    const midBins = this.dataArray.slice(50, 100);
+    const highBins = this.dataArray.slice(100, 150);
+    const midAvg = midBins.reduce((a, b) => a + b, 0) / midBins.length;
+    const highAvg = highBins.reduce((a, b) => a + b, 0) / highBins.length;
+    return (midAvg + highAvg) / 2;
+  }
+}
